3f5a6ebb65a8eec2cfd06606d00c7c997f5b0c3d
Garret Raziel <boloomka@gmail.com>
2012-04-12 19:56:13 +0200

    poznamka k tomu, jak bude omezeni vypadat

--- constraint.cpp
+++ constraint.cpp
@@ -7,5 +7,10 @@ Constraint::Constraint()
 StringToPntypeMap Constraint::chooseValues(StringToTokensMap possible_values)
 {
     StringToPntypeMap results;
+    //todo: backtrackingem prohleda possible_values
+    // pracuje se s tim takto: je to slovnik, kde vzdy na pozici jmena promenne ("x")
+    // je ulozen vektor hodnot, co ta promenna muze nabyvat
+
+    //vysledkem je mapa string na hodnotu, napr. "x" : 5. pokud neni mozno, vysledek je nulove delky
     return results;
 }
