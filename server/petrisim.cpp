8730ef70f8cbbd7bf5af32329fe68bcd9e52ad0d
lukassparrow <lukas.sparrow@gmail.com>
2012-05-02 12:50:35 +0200

    kopie z masteru

---
+++ server/petrisim.cpp
@@ -0,0 +1,67 @@
+#include "petrisim.h"
+#include "pnplace.h"
+#include "pntrans.h"
+#include <QtGlobal>
+#include <QTime>
+
+int rand_int(int low, int high)
+{
+    return qrand() % (high - low) + low;
+}
+
+PetriSim::PetriSim()
+{
+    QTime time = QTime::currentTime();
+    qsrand((uint)time.msec());
+}
+
+QString PetriSim::getState()
+{
+    return state.getState();
+}
+
+bool PetriSim::setState(QString xml)
+{
+    //todo: vse co je potreba pro nastaveni stavu
+    return this -> state.setState(xml);
+}
+
+void PetriSim::step()
+{
+    TransVector transits = state.getTransits();
+
+    TransVector::iterator it;
+
+    bool was_fired = false;
+
+    for (it = transits.begin(); it<transits.end(); it++) {
+        if ((*it)->fire()) {
+            (*it)->doOperations(0);
+            was_fired = true;
+        }
+    }
+}
+
+void PetriSim::run(int steps)
+{
+    for (int i = 0; i<steps; i++) {
+        TransVector transits = state.getTransits();
+        TransVector::iterator it;
+        TransVector possible_transits;
+
+        bool was_fired = false;
+        for (it = transits.begin(); it != transits.end(); it++) {
+            if ((*it)->fire()) {
+                possible_transits.push_back((*it));
+                was_fired = true;
+            }
+        }
+
+        if (!was_fired) return;
+
+        int which_transit = rand_int(0,possible_transits.size());
+        int which_values = rand_int(0,possible_transits[which_transit]->possibleChoicesCount());
+
+        possible_transits[which_transit]->doOperations(which_values);
+    }
+}
---
+++ server/petrisim.h
@@ -0,0 +1,18 @@
+#ifndef PETRISIM_H
+#define PETRISIM_H
+
+#include "simstate.h"
+
+class PetriSim
+{
+    SimState state; //stav simulace
+public:
+    PetriSim();
+    void step();
+    void run(int steps = 100);
+
+    QString getState(); //vrati stav pro ulozeni nebo prenos
+    bool setState(QString xml); // nastavi stav, pro nahrani simulace
+};
+
+#endif // PETRISIM_H
