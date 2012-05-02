46c4733175acd8a2737972720f94cee0d4289581
Garret Raziel <boloomka@gmail.com>
2012-05-02 11:58:00 +0200

    Revert "procisteno"
    This reverts commit 54324d02b2c69ba67526e8bd018ba869cf92fa43.

---
+++ server/main.cpp
@@ -0,0 +1,19 @@
+#include <QtCore/QCoreApplication>
+#include "petrinetserver.h"
+#include "petrisim.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    
+    PetriNetServer *server = new PetriNetServer(0,50,"127.0.0.1",11899);
+
+    bool is_running = server->start();
+
+    if (!is_running) {
+        qCritical() << "Cannot start server\n";
+        return 1; //todo: chyby nejak lip
+    }
+
+    return a.exec();
+}
---
+++ server/server.pro
@@ -0,0 +1,26 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-04-09T22:04:14
+#
+#-------------------------------------------------
+
+QT       += core network
+
+QT       -= gui
+
+TARGET = server
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    petrinetserver.cpp \
+    petrisim.cpp
+
+HEADERS += \
+    petrinetserver.h \
+    petrisim.h
+
+
