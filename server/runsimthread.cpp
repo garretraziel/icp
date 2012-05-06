4b1179b0e4ba43e0dc7dce13f42e99fa5ff3b8a0
Garret Raziel <boloomka@gmail.com>
2012-05-06 09:15:56 +0200

    oprava male chybky

--- server/runsimthread.cpp
+++ server/runsimthread.cpp
@@ -1,7 +1,7 @@
 #include "runsimthread.h"
 #include <QDebug>
 
-RunSimThread::RunSimThread(QString myid, PetriSim *sim, QMutex *outmutex, QString *outid, bool run, QObject *parent) :
+RunSimThread::RunSimThread(QString myid, PetriSim *sim, QMutex *outmutex, QString *outid, bool run_or_step, QObject *parent) :
     QThread(parent),myid(myid), sim(sim), outmutex(outmutex), outid(outid), run_or_step(run_or_step)
 {
 }
