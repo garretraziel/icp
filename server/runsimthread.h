58f5c7c112577f207566c29481d4364cb64107ba
Garret Raziel <boloomka@gmail.com>
2012-05-06 18:59:12 +0200

    runsimthread okomentovan

--- server/runsimthread.h
+++ server/runsimthread.h
@@ -1,3 +1,13 @@
+/**
+ * @file runsimthread.h
+ * @author Jan Sedlak <xsedla85@stud.fit.vutbr.cz>
+ * @author Lukas Brabec <xbrabe09@stud.fit.vutbr.cz>
+ *
+ * @section DESCRIPTION
+ *
+ * Vlakno, ve kterem bezi samotna simulace.
+ */
+
 #ifndef RUNSIMTHREAD_H
 #define RUNSIMTHREAD_H
 
@@ -6,18 +16,34 @@
 #include <QString>
 #include "petrisim.h"
 
+/**
+ * Trida, vlakno, ve kterem bezi samotna jedna spustena simulace.
+ */
 class RunSimThread : public QThread
 {
     Q_OBJECT
 public:
+    /**
+      * Konstruktor vlakna - nastavi zadane atributy.
+      *
+      * @param myid id simulace, ktera se zde bude spoustet
+      * @param sim ukazatel na simulaci, ktera se ma spustit
+      * @param outmutex semafor pro cekani na prebrani hodnoty dokonceni simulace
+      * @param outid adresa id, kam se zapise id dokoncene simulace
+      * @param run_or_step true pokud run, jinak false
+      * @param parent ukazatel na rodice typu qobject
+      */
     explicit RunSimThread(QString myid, PetriSim *sim, QMutex *outmutex, QString *outid, bool run_or_step, QObject *parent = 0);
+    /**
+      * Hlavni provadeci metoda vlakna, volana automaticky pri thread->start().
+      */
     void run();
 
-    QString myid;
-    PetriSim *sim;
-    QMutex *outmutex;
-    QString *outid;
-    bool run_or_step;
+    QString myid; ///< id simulace, ktera se zde bude spoustet
+    PetriSim *sim; ///< sim ukazatel na simulaci, ktera se ma spustit
+    QMutex *outmutex; ///< outmutex semafor pro cekani na prebrani hodnoty dokonceni simulace
+    QString *outid; ///< adresa id, kam se zapise id dokoncene simulace
+    bool run_or_step; ///< true pokud run, jinak false
 signals:
     
 public slots:
