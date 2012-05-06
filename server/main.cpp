2cad39e75d9def3e36705a20637857fc783358a7
Garret Raziel <boloomka@gmail.com>
2012-05-06 19:10:21 +0200

    okomentovana funkce main

--- server/main.cpp
+++ server/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Jan Sedlak <xsedla85@stud.fit.vutbr.cz>
+ * @author Lukas Brabec <xbrabe09@stud.fit.vutbr.cz>
+ *
+ * @section DESCRIPTION
+ *
+ * Soubor s hlavni funkci main aplikace.
+ * Vytvori se server, necha se naslouchat na portu 11899 a spusti se event-loop Qt.
+ */
+
 #include <QtCore/QCoreApplication>
 #include "petrinetserver.h"
 #include "petrisim.h"
@@ -5,11 +16,13 @@
 #include <QTextStream>
 #include <QDebug>
 
+#define PORTNUMBER 11899
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     
-    PetriNetServer server(0,50,11899);
+    PetriNetServer server(0,50,PORTNUMBER);
 
     bool is_running = server.start();
 
