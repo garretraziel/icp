894032c76ebf14cd81ae6fb7a7f6e96b04554a85
Garret Raziel <boloomka@gmail.com>
2012-05-06 13:30:37 +0200

    server procistenej

--- server/main.cpp
+++ server/main.cpp
@@ -9,45 +9,14 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     
-    PetriNetServer *server = new PetriNetServer(0,50,11899);
+    PetriNetServer server(0,50,11899);
 
-    bool is_running = server->start();
+    bool is_running = server.start();
 
     if (!is_running) {
         qCritical() << "Cannot start server\n";
-        return 1; //todo: chyby nejak lip
-    }
-
-    QFile soubor("semafor.xml");
-
-    if(!soubor.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qCritical("Cannot open file.");
         return 1;
     }
-
-    QString str;
-    QTextStream stream(&soubor);
-
-    while (!stream.atEnd()) {
-        str += stream.readLine();
-    }
-
-    soubor.close();
-
-    PetriSim *simulace = new PetriSim;
-
-    simulace->setState(str);
-
-    qDebug() << simulace->getState();
-
-    simulace->run();
-
-    qDebug() << simulace->getState();
-
-    //qDebug() << "trololo";
-
-    delete simulace;
-    //delete server;
 
     return a.exec();
 }
