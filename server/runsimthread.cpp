afc9539b889fb143e57e8b3abaf513619d37133b
Garret Raziel <boloomka@gmail.com>
2012-05-06 11:03:59 +0200

    lepsi srandovani

--- server/runsimthread.cpp
+++ server/runsimthread.cpp
@@ -1,5 +1,8 @@
 #include "runsimthread.h"
 #include <QDebug>
+#include <time.h>
+#include <QtGlobal>
+#include <QTime>
 
 RunSimThread::RunSimThread(QString myid, PetriSim *sim, QMutex *outmutex, QString *outid, bool run_or_step, QObject *parent) :
     QThread(parent),myid(myid), sim(sim), outmutex(outmutex), outid(outid), run_or_step(run_or_step)
@@ -8,6 +11,8 @@ RunSimThread::RunSimThread(QString myid, PetriSim *sim, QMutex *outmutex, QStrin
 
 void RunSimThread::run()
 {
+    //qsrand(time(NULL));
+    qsrand(QTime::currentTime().msec());
     qDebug() << "[info] starting: " << myid;
     if (run_or_step) {
         sim->run();
