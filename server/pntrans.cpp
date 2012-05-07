f882ef2fb2501b7de9dc2f524d03b2f4baa56c1a
Garret Raziel <boloomka@gmail.com>
2012-05-07 14:02:57 +0200

    trans hurr durr

--- server/pntrans.cpp
+++ server/pntrans.cpp
@@ -14,10 +14,16 @@
 
 typedef std::vector<int> IntVector;
 
+/**
+  * Konstruktor mista.
+  */
 PNTrans::PNTrans()
 {
 }
 
+/**
+  * Destruktor tridy, uvolni to, co je potreba.
+  */
 PNTrans::~PNTrans()
 {
     ConstraintVector::iterator cit;
@@ -28,6 +34,11 @@ PNTrans::~PNTrans()
     constraints.clear();
 }
 
+/**
+  * Zjisti, zda by prechod sel odpalit.
+  *
+  * @return true pokud odpalit jde, jinak false
+  */
 bool PNTrans::fire()
 {
     StringToPnplaceMap::iterator it;
@@ -47,6 +58,13 @@ bool PNTrans::fire()
     return true;
 }
 
+/**
+  * Zjisti, ktere z kombinaci vstupnich promennych vyhovuji podminkam, vysledek vrati a ulozi
+  *
+  * @param hash slovnik poli ve tvaru {"x":[1,2,3],"y":[4,5,6,...]}
+  *
+  * @return vektor slovniku moznych kombinaci odpaleni
+  */
 StrPntMapVector PNTrans::chooseValues(StringToTokensMap hash)
 {
     StrPntMapVector result; //vektor moznych kombinaci hodnot, co odpovidaji podminkam
@@ -104,6 +122,13 @@ StrPntMapVector PNTrans::chooseValues(StringToTokensMap hash)
     return result; //vratim kombinace, co odpovidaji
 }
 
+/**
+  * Provede vybranou operaci podle vektoru moznych konfiguraci.
+  *
+  * @param choice vyber, ktera kombinace se ma provest
+  *
+  * @return true pokud se povedl prechod odpalit, jinak false
+  */
 bool PNTrans::doOperations(unsigned int choice)
 {
     if (choice >= possible_choices.size()) return false; //vybrana kombinace neexistuje
@@ -147,11 +172,22 @@ bool PNTrans::doOperations(unsigned int choice)
     return true;
 }
 
+/**
+  * Vrati pocet moznych konfiguraci k odpaleni.
+  *
+  * @return pocet konfiguraci
+  */
 int PNTrans::possibleChoicesCount()
 {
     return possible_choices.size(); //vratim velikost moznosti
 }
 
+/**
+  * Odstrani zadane napojene misto.
+  *
+  * @param place ukazatel na misto, ktere se ma odebrat
+  * @param isIn jestli ma byt vstupni
+  */
 void PNTrans::removeConnectedPlace(PNPlace * place, bool isIn){
     StringToPnplaceMap * names = (isIn)? &in_names : &out_names;
     StringToPnplaceMap::iterator it;
