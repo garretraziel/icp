7f036e7c9350131ed52f5192e0d6c9734c396a4b
Garret Raziel <boloomka@gmail.com>
2012-05-07 14:04:02 +0200

    td jsm kmntvl rnsmthrd.cpp

--- server/runsimthread.cpp
+++ server/runsimthread.cpp
@@ -14,11 +14,24 @@
 #include <QtGlobal>
 #include <QTime>
 
+/**
+  * Konstruktor vlakna - nastavi zadane atributy.
+  *
+  * @param myid id simulace, ktera se zde bude spoustet
+  * @param sim ukazatel na simulaci, ktera se ma spustit
+  * @param outmutex semafor pro cekani na prebrani hodnoty dokonceni simulace
+  * @param outid adresa id, kam se zapise id dokoncene simulace
+  * @param run_or_step true pokud run, jinak false
+  * @param parent ukazatel na rodice typu qobject
+  */
 RunSimThread::RunSimThread(QString myid, PetriSim *sim, QMutex *outmutex, QString *outid, bool run_or_step, QObject *parent) :
     QThread(parent),myid(myid), sim(sim), outmutex(outmutex), outid(outid), run_or_step(run_or_step)
 {
 }
 
+/**
+  * Hlavni provadeci metoda vlakna, volana automaticky pri thread->start().
+  */
 void RunSimThread::run()
 {
     qsrand(QTime::currentTime().msec()); //pro kazde vlakno musim volat srand zvlast
